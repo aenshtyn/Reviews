b17a9b4bd63b04fd4863d3af93ae50a3e42d646c
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-06-08 08:45:49 +0300

    created the first model Project

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Project (models.Model):
+    name = models.CharField(max_length =30)
+    languages = models.CharField(max_length =30)
+    author = models.CharField(max_length =30)
+    pub_date = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.first_name
+
+    class Meta:
+        ordering = ['first_name']

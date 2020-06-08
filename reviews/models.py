459abf9a2ef1b540752d7dc3c51061d301bab486
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-06-08 09:12:14 +0300

    created the Author models and language models

--- reviews/models.py
+++ reviews/models.py
@@ -1,17 +1,25 @@
 from django.db import models
 
 # Create your models here.
-class Project (models.Model):
-    name = models.CharField(max_length =30)
-    languages = models.CharField(max_length =30)
-    author = models.CharField(max_length =30)
-    pub_date = models.CharField(max_length =30)
+class Author(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
 
     def __str__(self):
         return self.first_name
 
+class Project (models.Model):
+    name = models.CharField(max_length =30)
+    languages = models.ManyToManyField(languages)
+    author = models.ForeignKey(Author)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
     class Meta:
-        ordering = ['first_name']
+        ordering = ['name']
 
 class languages(models.Model):
     name = models.CharField(max_length =30)
